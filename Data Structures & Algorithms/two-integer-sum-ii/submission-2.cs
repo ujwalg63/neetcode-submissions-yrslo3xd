dfa09380f28574ffe3816e58aa8c648d26989335
ujwalg63 <144614933+ujwalg63@users.noreply.github.com>
2026-04-14 22:46:50 -0400

    Add: two-integer-sum-ii - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-2.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public int[] TwoSum(int[] numbers, int target) {
+        int i=0, j = numbers.Length -1;
+        while(i<j)
+        {
+            if(numbers[i] + numbers[j] == target)
+            {
+                return new int[]{i+1, j+1};
+            }
+            if(numbers[i] + numbers[j] < target)
+            {
+                i++;
+            }
+            if(numbers[i] + numbers[j] > target)
+            {
+                j--;
+            }
+        }
+        return new int[0];
+    }
+}
